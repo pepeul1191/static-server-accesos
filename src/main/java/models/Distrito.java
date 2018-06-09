8ce20a326cd89026a08ec53df053d4441e5cc7f1
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-09 08:47:41 -0500

    Nuevo modelos añadidos

---
+++ src/main/java/models/Distrito.java
@@ -0,0 +1,8 @@
+package models;
+import org.javalite.activejdbc.Model;
+//import org.javalite.activejdbc.annotations.DbName;
+import org.javalite.activejdbc.annotations.Table;
+
+//@DbName("ubicaciones")
+@Table("distritos")
+public class Distrito extends Model {}
---
+++ src/main/java/models/VWDistritoProvinciaDepartamento.java
@@ -0,0 +1,8 @@
+package models;
+import org.javalite.activejdbc.Model;
+//import org.javalite.activejdbc.annotations.DbName;
+import org.javalite.activejdbc.annotations.Table;
+
+//@DbName("ubicaciones")
+@Table("vw_distrito_provincia_departamento")
+public class VWDistritoProvinciaDepartamento extends Model {}
