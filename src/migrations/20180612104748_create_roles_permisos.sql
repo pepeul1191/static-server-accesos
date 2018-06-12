95ac71717a87b696d0e9d4a0fad87c4b289593c1
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-12 10:50:29 -0500

    Migración, crear y llenar roles_permisos

---
+++ src/migrations/20180612104748_create_roles_permisos.sql
@@ -0,0 +1,10 @@
+--DOWN
+DROP TABLE IF EXISTS roles_permisos;
+--UP
+CREATE TABLE roles_permisos(
+	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  rol_id INTEGER,
+  permiso_id INTEGER,
+  FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE,
+  FOREIGN KEY (permiso_id) REFERENCES permisos(id) ON DELETE CASCADE
+)
---
+++ src/migrations/20180612104805_fill_roles_permisos.sql
@@ -0,0 +1,9 @@
+--DOWN
+--TRUNCATE TABLE roles_permisos;
+--UP
+INSERT INTO roles_permisos (rol_id, permiso_id) VALUES (
+  1,1
+);
+INSERT INTO roles_permisos (rol_id, permiso_id) VALUES (
+  1,2
+);
