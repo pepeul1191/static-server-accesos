66c3db86b17daacaaf5d49c099b8cee038ffa944
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-11 22:10:54 -0500

    Crear y llenar items

---
+++ src/migrations/20180611220615_fill_items.sql
@@ -0,0 +1,13 @@
+--DOWN
+--TRUNCATE TABLE items;
+--UP
+INSERT INTO items (nombre, url, subtitulo_id) VALUES (
+  'Gestión de Sistemas',
+  '#/sistema',
+  1
+);
+INSERT INTO items (nombre, url, subtitulo_id) VALUES (
+  'Gestión de Usuarios',
+  '#/usuario',
+  1
+);
